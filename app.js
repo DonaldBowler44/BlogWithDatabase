f896f0b7644f2292c471f0b253ad90a0abe94287
DonaldBowler44 <96385571+DonaldBowler44@users.noreply.github.com>
2022-02-28 10:46:13 -0600

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("SERHOST/DATABASENAME", {useNewUrlParser: true});
 
 const postSchema = {
 	title: String,
